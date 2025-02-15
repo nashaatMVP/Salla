ae9526d87a745cc6da8a5d8beabfeeb26802fc41
nashaatMVP <mahmoud.kamal@mvp-apps.ae>
2025-02-15 16:33:41 +0400

    any changes

--- lib/main.dart
+++ lib/main.dart
@@ -52,10 +52,12 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
